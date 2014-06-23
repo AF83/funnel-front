0bc0df3c5ddf1c412670222a1afaffbadaa6f657
Damien Ragoucy <damien.ragoucy@af83.com>
2014-06-23 22:01:11 +0200

    Initialize SSE on application boot

---
+++ src/cljs/funnel_front/actions/app_actions.cljs
@@ -0,0 +1,8 @@
+(ns funnel-front.actions.app-actions
+
+  (:use [funnel-front.lib.helpers :only [clog!]]))
+
+
+(defn update-app-state [event]
+  ;; TODO
+  (clog! "Funnel event"))
--- src/cljs/funnel_front/core.cljs
+++ src/cljs/funnel_front/core.cljs
@@ -1,13 +1,19 @@
 (ns funnel-front.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true])
+            [om.dom :as dom :include-macros true]
+            [funnel-front.actions.app-actions :as actions])
 
   (:use [funnel-front.components.home.main :only [main-comp]]
+        [funnel-front.lib.helpers :only [event-source]]
         [funnel-front.stores.main-store :only [app-state]]))
 
 (enable-console-print!)
 
+;; Boot app
 (om/root
   main-comp
   app-state
   {:target (. js/document (getElementById "app"))})
+
+;; Initialize SSE
+(event-source "/events" actions/update-app-state)
