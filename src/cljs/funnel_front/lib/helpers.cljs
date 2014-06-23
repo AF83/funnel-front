ce9a62ae69bfbe35c608dcd0ff1affa1c2376139
Damien Ragoucy <damien.ragoucy@af83.com>
2014-06-23 21:33:56 +0200

    Added Event Source wrapper

--- src/cljs/funnel_front/lib/helpers.cljs
+++ src/cljs/funnel_front/lib/helpers.cljs
@@ -3,3 +3,8 @@
 ;; https://groups.google.com/forum/#!topic/clojure/ghHqHuXE0pY
 (defn clog! [to-print]
   (.log js/console (pr-str to-print)))
+
+(defn event-source [route callback]
+  (let [source (js/EventSource. route)]
+    (set! (.-onmessage source) callback)
+    source))
