14e227303db25e520ec6c13a2e9914a5a659f66d
Damien Ragoucy <damien.ragoucy@af83.com>
2014-07-20 14:19:49 +0200

    Added panel pictos

--- src/cljs/funnel_front/components/home/panel.cljs
+++ src/cljs/funnel_front/components/home/panel.cljs
@@ -7,5 +7,15 @@
   (reify
     om/IRender
     (render [this]
-      (dom/div #js {:className "panel"}
-        (dom/h1 nil (str "Token: " (:token app)))))))
+      (dom/div #js {:id "panel"}
+        (dom/a #js {:href "#"}
+          (dom/img #js {:src "/images/logo.png" :alt "Funnel Front Logo"}))
+        (dom/a #js {:href "#"}
+          (dom/img #js {:src "/images/user.png" :alt "User Picto"}))
+        (dom/a #js {:href "#"}
+          (dom/img #js {:src "/images/search.png" :alt "Search Picto"}))
+        (dom/a #js {:href "#"}
+          (dom/img #js {:src "/images/list.png" :alt "List Picto"}))
+        (dom/a #js {:href "#"}
+          (dom/img #js {:src "/images/add.png" :alt "Add Picto"}))
+        ))))
