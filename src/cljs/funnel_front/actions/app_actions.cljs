a3d5d955cbc2af9de9c2ecd02c418b17a75511ec
Damien Ragoucy <damien.ragoucy@af83.com>
2014-08-03 23:04:43 +0200

    Simplified app state update
    But still WIP

--- src/cljs/funnel_front/actions/app_actions.cljs
+++ src/cljs/funnel_front/actions/app_actions.cljs
@@ -5,15 +5,12 @@
 
 
 (defn update-app-state [event]
-  (let [data (.parse js/JSON (.-data event))
-        queries (.-queries data)
-        body (.parse js/JSON (.-body data))]
-    (.log js/console data)
-    (.log js/console "first match :" (.-id (first queries)))
-    (swap! app-state
-           update-in
-           [:queries (keyword (.-id (first queries))) :items]
-           conj {:body (js->clj body)})
-    (.log js/console @app-state)
-    ))
-
+  (let [data (->> event (.-data) (js->clj) )]
+    (clog! data)
+    ;; app state update is in work in progress
+    #_(swap! app-state
+             update-in
+             [:queries (keyword (.-id (first queries))) :items]
+             conj {:body (js->clj body)})
+    )
+  )
