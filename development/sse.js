39490ff5e31ebed52a3cf6fb757bd8568ce1cb84
Damien Ragoucy <damien.ragoucy@af83.com>
2014-08-03 17:00:56 +0200

    Updated fake Node.js SSE

--- development/sse.js
+++ development/sse.js
@@ -2,43 +2,7 @@ var http = require('http');
 var sys = require('sys');
 var fs = require('fs');
 
-http.createServer(function(req, res) {
-  debugHeaders(req);
-
-  if (req.headers.accept &&
-      req.headers.accept == 'text/event-stream' &&
-      req.url == '/events')
-  {
-    sendSSE(req, res);
-  } else {
-    res.writeHead(404);
-    res.end();
-  }
-
-}).listen(8000);
-
-console.log("started listening on port 8000");
-
-function sendSSE(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  var id = (new Date()).toLocaleTimeString();
-
-  setInterval(function() {
-    constructSSE(res, id, (new Date()).toLocaleTimeString());
-  }, 5000);
-
-  constructSSE(res, id, (new Date()).toLocaleTimeString());
-}
-
-function constructSSE(res, id, data) {
-  res.write('id: ' + id + '\n');
-  res.write("data: " + data + '\n\n');
-}
+var PORT = 4000;
 
 function debugHeaders(req) {
   console.log('URL: ' + req.url);
@@ -47,3 +11,56 @@ function debugHeaders(req) {
   }
   console.log('\n\n');
 }
+
+function createData() {
+  return {
+    time: (new Date()).toLocaleTimeString(),
+    queries: [
+      {type: "github"},
+      {type: "trello"}
+    ]
+  };
+}
+
+function constructSSE(res, id, data) {
+  var idText = 'id: ' + id + '\n',
+      dataText = "data: " + JSON.stringify(createData()) + '\n\n';
+
+  console.log(idText + dataText);
+
+  res.write(idText);
+  res.write(dataText);
+}
+
+function sendSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  var id = Date.now();
+
+  setInterval(function() {
+    constructSSE(res, id, createData());
+  }, 1000);
+
+  constructSSE(res, id, createData());
+}
+
+http.createServer(function(req, res) {
+  debugHeaders(req);
+
+  if (req.headers.accept &&
+      req.headers.accept == 'text/event-stream' &&
+      req.url.match(/river/))
+  {
+    sendSSE(req, res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+
+}).listen(PORT);
+
+console.log("started listening on port " + PORT);
