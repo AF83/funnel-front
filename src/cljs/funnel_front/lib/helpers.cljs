02793533a663acbd6babbbd6cd9f3fef4f20d249
Damien Ragoucy <damien.ragoucy@af83.com>
2014-08-03 22:57:58 +0200

    clog! helper accepts multiple arguments

--- src/cljs/funnel_front/lib/helpers.cljs
+++ src/cljs/funnel_front/lib/helpers.cljs
@@ -1,10 +1,11 @@
 (ns funnel-front.lib.helpers)
 
 ;; https://groups.google.com/forum/#!topic/clojure/ghHqHuXE0pY
-(defn clog! [to-print]
-  (.log js/console (pr-str to-print)))
+(defn clog! [& items-to-print]
+  (.log js/console (apply pr-str items-to-print)))
 
 (defn event-source [route callback]
   (let [source (js/EventSource. route)]
     (set! (.-onmessage source) callback)
-    source))
+    source
+    ))
