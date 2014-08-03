08d97525ee05d8044f44df50def6c9321dbf6501
Damien Ragoucy <damien.ragoucy@af83.com>
2014-08-03 23:03:24 +0200

    Removed namespace on function use

--- src/cljs/funnel_front/core.cljs
+++ src/cljs/funnel_front/core.cljs
@@ -1,10 +1,10 @@
 (ns funnel-front.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [funnel-front.actions.app-actions :as actions])
+            [om.dom :as dom :include-macros true])
 
   (:use [funnel-front.components.home.main :only [main-comp]]
         [funnel-front.lib.helpers :only [event-source]]
+        [funnel-front.actions.app-actions :only [update-app-state]]
         [funnel-front.stores.main-store :only [app-state]]))
 
 (enable-console-print!)
@@ -16,5 +16,4 @@
   {:target (. js/document (getElementById "app"))})
 
 ;; Initialize SSE
-(event-source "/river?token=635a3366bbdd4dfaba9df415e51a60e0" actions/update-app-state)
-
+(event-source "/river?token=635a3366bbdd4dfaba9df415e51a60e0" update-app-state)
